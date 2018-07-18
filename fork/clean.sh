2f350cc588aa6e1983bdfd1ea6da89c66bd91ffb
hirsei <p.lachmund@hortpro.de>
2018-07-18 11:03:27 +0200

    edit clean script

--- fork/clean.sh
+++ fork/clean.sh
@@ -1,3 +1,7 @@
+echo "Removing Cassandra Cluster Service"
+docker service rm cassandra
+sleep 5
+echo "Cleaning Containers and Volumes"
 docker system prune -f
 docker volume prune -f
 
--- fork/create.sh
+++ fork/create.sh
@@ -9,14 +9,17 @@ docker service create \
   -e MAX_HEAP_SIZE=64M \
   192.168.99.100:5000/casfork
 
-if [ $1 == "" || $1 == "1"]
+if [ $1 == "" ]
   then
     echo "To scale service manually type: docker service scale cassandra=<number of replicas>"
   else
-    for ((i=2;i<=$1;i++))
-    do
-      echo "Scaling up to $i instances"
-	  sleep 20
-      docker service scale cassandra=$i
-    done
+    if [ $1 == "1" ]
+	else 
+		for ((i=2;i<=$1;i++))
+		do
+		  echo "Scaling up to $i instances"
+		  sleep 20
+		  docker service scale cassandra=$i
+		done
+	fi
 fi

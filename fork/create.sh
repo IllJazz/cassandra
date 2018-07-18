17c316b46e6f507453bbd0230ebc2efdddde88a5
hirsei <p.lachmund@hortpro.de>
2018-07-18 10:47:26 +0200

    edit create script

--- fork/create.sh
+++ fork/create.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+# Cassandra Cluster StartUp Script
+echo "Starting Single Cassandra Node"
 docker network create -d overlay --attachable casnet
 docker service create \
   --name cassandra \
@@ -5,3 +8,15 @@ docker service create \
   -e HEAP_NEWSIZE=12M \
   -e MAX_HEAP_SIZE=64M \
   192.168.99.100:5000/casfork
+
+if [ $1 == "" || $1 == "1"]
+  then
+    echo "To scale service manually type: docker service scale cassandra=<number of replicas>"
+  else
+    for ((i=2;i<=$1;i++))
+    do
+      echo "Scaling up to $i instances"
+	  sleep 20
+      docker service scale cassandra=$i
+    done
+fi

ff7261aeff1fd5a83c8e32ec464d3aa543fd94a5
hirsei <p.lachmund@hortpro.de>
2018-08-06 09:04:16 +0200

    linefeeds

--- fork/create.sh
+++ fork/create.sh
@@ -9,7 +9,6 @@ docker service create \
   -e HEAP_NEWSIZE=12M \
   -e MAX_HEAP_SIZE=64M \
   192.168.99.100:5000/casfork
-
 if [ $1 == "" ]
   then
     echo "To scale service manually type 'docker service scale cassandra=<number of replicas>'"
